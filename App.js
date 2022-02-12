38822f4f26f71edbd26529546bfe2ddf66ffa45d
Badwolf <rubancar@espol.edu.ec>
2022-02-12 19:43:32 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View>
+        <Text style={styles.welcome_text}>!Bienvenido¡</Text>
+
+        <View style={styles.view_input}>
+          <Text style={styles.label_input}>Usuario</Text>
+          <TextInput style={styles.input} />
+        </View>
+
+        <View style={styles.view_input}>
+          <Text style={styles.label_input}>Contraseña</Text>
+          <TextInput style={styles.input} />
+        </View>
+
+        <Button 
+          title = "Entrar" 
+          onPress = {() => Alert.alert("Entrar!")}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +32,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection:"column",
     justifyContent: 'center',
+    padding: 40
   },
+  welcome_text: {
+    marginBottom: 20,
+   fontSize: 30,
+   fontWeight: "bold",
+   textAlign: "center"
+  },
+  label_input: {
+    fontSize: 15,
+    fontWeight: "bold",
+    paddingBottom: 12
+  },
+  input: {
+    height: 25,
+    marginBottom: 10,
+    borderWidth: 1,
+    width: "65%"
+  },
+  view_input: {
+    flexDirection:"row",
+    justifyContent: "space-between",
+    alignItems: "flex-end"
+  }
 });
