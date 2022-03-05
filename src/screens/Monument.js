aee9f04d5962c7f5aac4239a0ca938b2ed674a01
Matojo <matojo@uv.es>
2022-03-05 20:39:12 +0100

    Añadida nueva funcionalidad: Listado monumentos 1.0

--- src/screens/Monument.js
+++ src/screens/Monument.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { Text, View, Button, StyleSheet,Image } from 'react-native';
 import { StackActions } from '@react-navigation/native';
 
-const Monument = ({ navigation }) => {
+const Monument = ({ navigation, route }) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Detalle del monumento de la falla</Text>
+            <Text>Detalle del monumento de la falla {route.params.infoMonument.nombre}</Text>
+            <Text>Sección {route.params.infoMonument.seccion}</Text>
+            <Text>Fallera {route.params.infoMonument.fallera}</Text>
+            <Text>Presidente {route.params.infoMonument.presidente}</Text>
+            <Image style={styles.tinyLogo}  source={{uri: route.params.infoMonument.boceto }}/>
         </View>
     );
 };
 
+const styles = StyleSheet.create({
+    tinyLogo: {
+      width: 500,
+      height: 500,
+    },
+  });
 
 export default Monument;
